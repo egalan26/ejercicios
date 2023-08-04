f53f1bcca971c801a508a751270c8ed5861a6958
egalan26 <erodriguezgalan@gmail.com>
2023-08-04 13:28:42 +0200

    comentario

--- ejercicio2/test.py
+++ ejercicio2/test.py
@@ -1,6 +1,7 @@
 from main import func1
 
 
+# debes resolver func1 para que la respuesta sea correcta
 
 print("El resultado de esta operación es 256")
 print("Tu solución es "+ ('correcta' if func1(2,8) == 256 else 'incorrecta'))
